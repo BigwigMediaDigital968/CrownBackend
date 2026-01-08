4945bf5378b22293c984145710656c64b55f5c57
shubham9049 <shubham.rajveer19@gmail.com>
2026-01-08 18:26:29 +0530

    add changes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const { connect } = require("./config/db");
-// const subscriberRoutes = require("./routes/subscriber.route");
-// const leadRoutes = require("./routes/leads.route");
-// const blogRoutes = require("./routes/blog.route");
+const subscriberRoutes = require("./routes/subscriber.route");
+const leadRoutes = require("./routes/leads.route");
+const blogRoutes = require("./routes/blog.route");
 const buyproperty = require("./routes/property.route");
-// const sellproperty = require("./routes/sell.route");
-// const sellApproval = require("./routes/adminApproval");
-// const brochureLeadRoutes = require("./routes/brochureLead");
-// const plotRoutes = require("./routes/plot.route");
+const sellproperty = require("./routes/sell.route");
+const sellApproval = require("./routes/adminApproval");
+const brochureLeadRoutes = require("./routes/brochureLead");
+const plotRoutes = require("./routes/plot.route");
 
 require("dotenv").config();
 
@@ -17,14 +17,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use("/api", subscriberRoutes);
-// app.use("/api/lead", leadRoutes);
-// app.use("/blog", blogRoutes);
+app.use("/api", subscriberRoutes);
+app.use("/api/lead", leadRoutes);
+app.use("/blog", blogRoutes);
 app.use("/api/property", buyproperty);
-// app.use("/sellproperty", sellproperty);
-// app.use("/sell", sellApproval);
-// app.use("/brochure-leads", brochureLeadRoutes);
-// app.use("/plot", plotRoutes);
+app.use("/sellproperty", sellproperty);
+app.use("/sell", sellApproval);
+app.use("/brochure-leads", brochureLeadRoutes);
+app.use("/plot", plotRoutes);
 
 app.use("/", (req, res) => {
   res.send("API LIVE🚀");
